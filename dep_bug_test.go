2adf76426d7323c0a6d2be217cc1823a9e8308dd
jgeiger <joey.geiger@irco.com>
2017-07-15 13:48:03 -0600

    Add initial testing

---
+++ dep_bug_test.go
@@ -0,0 +1,12 @@
+// Package main provides ...
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+	assert.Equal(t, 123, 123, "they should be equal")
+}
